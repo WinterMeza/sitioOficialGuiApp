60b516181af700da429ece518119435ba8d54b6f
Winter Aníbal Meza Jiménez <e1317233748@live.uleam.edu.ec>
2023-12-28 16:22:14 -0500

    segundo commit

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+console.log('¡Bienvenido a GuiApp!');
+
 // Obtén una referencia al botón de descarga
-var downloadButton = document.querySelector('.download-button');
+var downloadButton = document.getElementById('downloadButton');
 
 // Agrega un evento de clic al botón
 downloadButton.addEventListener('click', function() {
-    console.log('Botón de descarga clicado. Iniciar descarga o realizar otra acción aquí.');
+    // Muestra un mensaje superpuesto utilizando SweetAlert2
+    Swal.fire({
+        icon: 'success',
+        title: 'Descarga iniciada',
+        text: '¡Disfruta de GuiApp!',
+    });
+
     // Puedes agregar lógica adicional aquí, como iniciar la descarga de la aplicación.
 });
