c20c8c3b13a706bf9dc500947733ed3b35703c01
Winter Aníbal Meza Jiménez <e1317233748@live.uleam.edu.ec>
2023-12-28 16:14:02 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// Obtén una referencia al botón de descarga
+var downloadButton = document.querySelector('.download-button');
+
+// Agrega un evento de clic al botón
+downloadButton.addEventListener('click', function() {
+    console.log('Botón de descarga clicado. Iniciar descarga o realizar otra acción aquí.');
+    // Puedes agregar lógica adicional aquí, como iniciar la descarga de la aplicación.
+});
